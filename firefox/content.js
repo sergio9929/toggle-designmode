a1abe26857d1c4db4d7a4c5872a2d3aca279ba90
sergio <sergio9929@hotmail.com>
2023-04-25 02:45:39 +0200

    permission changes and browser compatibility

---
+++ firefox/content.js
@@ -0,0 +1,36 @@
+document.addEventListener('click', preventDefault, true)
+
+detectColorScheme()
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.click) {
+        toggleDesignMode()
+    }
+});
+
+function toggleDesignMode() {
+    document.documentElement.spellcheck = false
+    if (document.designMode == 'off') {
+        document.designMode = 'on'
+        chrome.runtime.sendMessage({ designMode: 'on' });
+    } else {
+        document.designMode = 'off'
+        chrome.runtime.sendMessage({ designMode: 'off' });
+    }
+}
+
+function preventDefault(event) {
+    if (document.designMode == 'on') {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+}
+
+function detectColorScheme() {
+    const isDark = matchMedia('(prefers-color-scheme: dark)').matches
+    chrome.runtime.sendMessage({ scheme: isDark ? 'dark' : 'light' })
+
+    matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches }) => {
+        chrome.runtime.sendMessage({ scheme: matches ? 'dark' : 'light' })
+    })
+}
